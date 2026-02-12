ea8918bd5da8f9480f4478a25d9221a49940e9ef
Dr Watson <>
2026-02-12 13:35:23 +0300

    android splash screen support

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -77,6 +77,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.splashscreen)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.testExt.junit)
     androidTestImplementation(libs.androidx.espresso.core)
